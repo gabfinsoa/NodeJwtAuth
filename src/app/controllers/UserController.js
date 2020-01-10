4242088409c21b929128b84c4886d459a5f20ce4
Gabriel Soares <gssoares@arezzo.com.br>
2020-01-10 09:07:57 -0300

    Add function checkPassword(password) {     return bcrypt.compare(password, this.password_hash);   }

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,13 +1,54 @@
+import User from '../models/User';
+
 class UserController {
   index() {}
 
   show() {}
 
-  store() {}
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
 
-  update() {}
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
 
-  delete() {}
+    // const { name, email } = req.body;
+
+    // criar usuario
+    const { name, email } = await User.create(req.body);
+
+    // Retornar o usuario
+    return res.json({
+      name,
+      email,
+    });
+  }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User alredy exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password invalid.' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+    });
+  }
 }
 
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,18 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import authMiddleware from './app/middlewares/auth';
+import AuthController from './app/controllers/AuthController';
 
 const routes = Router();
 
 routes.get('/', (req, res) => res.json('JWT-NODE'));
+
+routes.post('/users', UserController.store);
+routes.post('/login', AuthController.store);
+
+// Todas as rotas ABAIXO do .use, devem utilizar o TOKEN (bear authentication)
+routes.use(authMiddleware);
+
+routes.get('/test', (req, res) => res.json('Authenticaded'));
 
 export default routes;
